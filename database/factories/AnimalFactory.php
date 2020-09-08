74c0cfa20a75f9b66398ffd9becca0eeef18bde8
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-09-08 12:06:58 +0100

    delete button added with fetch request functionality added

--- database/factories/AnimalFactory.php
+++ database/factories/AnimalFactory.php
@@ -7,6 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Animal::class, function (Faker $faker) {
     return [
-        //
+        'owner_id' => factory(\App\Owner::class),
+        'name' => $faker->firstName,
+        'species' => $faker->randomElement(["Cow", "Bat", "Penguin", "Dog", "Hamster", "Cat", "Pig", "Lizard", "Snake", "Spider", "Horse", "Chicken"]),
+        'date_of_birth' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'weight' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 200),
+        'height' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 300),
+        'biteyness' => $faker->numberBetween($min = 0, $max = 5),
     ];
 });
--- resources/views/pages/owner.blade.php
+++ resources/views/pages/owner.blade.php
@@ -1,17 +1,17 @@
 @extends("app")
 
 @section("content")
-    <div class="card">
-        <h2 class="card-header">{{ $owner->fullAddress() }}</h2> 
+    <div class="card" id="delete-button">
+        <h2 class="card-header">{{ $owner->fullAddress() }}</h2>
+        <button>Delete</button>                           
         <article class="card-body">
              {{-- {{ /* if an article has comments list them */ }}  --}}
                 {{-- @if($owner->animals->isNotEmpty()) --}}
                      <div class="list-group">
                         @foreach ($owner->animals as $animal)
-                            <div class="d-flex w-100 justify-content-between"> 
+                            <div class="d-flex w-100 justify-content-between" > 
                                 <a href="pages/pets"><h5 class="mb-1">{{ $animal->name }}</h5></a>
                             </div>
-
                             {{-- <p class="mb-1">{{ $owner->animals }}</p>  --}}
                             @endforeach
                     </div> 
