74b8523d621aaa204f689ee1196be0b1029e1a3d
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-09-17 12:37:28 +0100

    fillable property added to animal model

---
+++ app/Animal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Animal extends Model
+{
+    // Mass Assignment Vulnerability; By adding this protected property to the data you are adding a layer of security to your database.
+    protected $fillable = ["name", "species", "date_of_birth", "weight", "height", "biteyness"];
+}
