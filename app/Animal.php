f3ec050299c2d54e0d676cdcc00afbde3ea4a3a4
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-09-17 12:39:34 +0100

    name and species properties added to animal model

--- app/Animal.php
+++ app/Animal.php
@@ -8,4 +8,12 @@ class Animal extends Model
 {
     // Mass Assignment Vulnerability; By adding this protected property to the data you are adding a layer of security to your database.
     protected $fillable = ["name", "species", "date_of_birth", "weight", "height", "biteyness"];
+    public function animalName()
+    {
+        return $this->name;
+    }
+    public function animalSpecies()
+    {
+        return $this->species;
+    }
 }
